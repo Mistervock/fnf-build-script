e0e6c553d76475cc85da14b704dca8f982a1931f
Mistervock <86741553+Mistervock@users.noreply.github.com>
2022-05-26 17:59:40 +0300

    Add files via upload

--- instructions.bat
+++ instructions.bat
@@ -9,4 +9,23 @@ ECHO 4. Put the build.cmd into %HAXELIB_ROOT% and open it
 pause
 ECHO 5. This cmd file will download all the necessary libraries by itself
 pause
-ECHO you can see credits.
+ECHO Q/A you want see? (about how to do something)
+ECHO 2. Yes
+ECHO 3. No
+set /p choice=choose, needed.
+if '%choice%'=='' ECHO "%choice%"  this is emtpy
+if '%choice%'=='2' goto yes
+if '%choice%'=='3' goto no
+ECHO.
+goto start
+:yes
+ECHO Q: where i download haxe?
+ECHO A: open main.py
+ECHO Q: do you like the auto?
+ECHO A: yes, i do
+goto end
+:no
+goto end
+:end
+pause
+exit
---
+++ main.py
@@ -0,0 +1,11 @@
+my_file = open("thanks.txt", "w+")
+my_file.write("thank you for downloading this tool")
+my_file.close()
+
+my_file = open("amogus.bat", "w+")
+my_file.write("start https://c.tenor.com/tqRFLhUBKjgAAAAC/get-real.gif")
+my_file.close()
+
+my_file = open("download haxe.cmd", "w+")
+my_file.write("start https://haxe.org/download/")
+my_file.close()
