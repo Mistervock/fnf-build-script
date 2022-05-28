7383d26d34391c2e2505a0244119a0819b0df75c
Mistervock <86741553+Mistervock@users.noreply.github.com>
2022-05-28 16:06:16 +0300

    Add files via upload

--- instructions.bat
+++ instructions.bat
@@ -3,7 +3,7 @@ ECHO 1. Go to your source code
 pause
 ECHO 2. Then you need write: cd (your way to the source example: C:\fnf-master)
 pause
-ECHO 3. Then write: haxelib setup "%HAXELIB_ROOT%"
+ECHO 3. Then write: haxelib setup %HAXELIB_ROOT%
 pause
 ECHO 4. Put the build.cmd into %HAXELIB_ROOT% and open it
 pause
